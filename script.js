bbb2e7b1374596045b86341b72d19515365e0ce0
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-05-15 10:39:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const menuBtn = document.querySelector('.menu-icon');
 
 function mostrarMenu(){
-    document.body.classList.toggle('ativo');
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle('ativo');
 }
 
 menuBtn.addEventListener('click', mostrarMenu);
 
    
+const carrrinho = document.querySelector('.img-cart')
+
+function popUp(){
+    const hoverCart = document.querySelector('.hoverCart');
+    hoverCart.classList.toggle('ativarPopup');
+}
+
+carrrinho.addEventListener('click', popUp);
